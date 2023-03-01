90cc9e297b8374b08f492dc61ae511a40669313a
Ana Matei <anamatei.099@gmail.com>
2023-03-02 00:27:16 +0100

    State location: App

--- books/src/App.js
+++ books/src/App.js
@@ -1,5 +1,14 @@
+import React from "react";
+import { useState } from "react";
+
 function App() {
+  const [books, setBooks] = useState([]);
+
   return <div>App</div>;
 }
 
 export default App;
+
+//State updated? Rerender the component it is defined in + all that components children
+//Find all the components that need to use this state
+//Define the state in the lowest common parent
